0d13e6707c870283ebd165bd91e3102c56f903dc
qxidddy <qxidddy@gmail.com>
2019-01-26 17:37:56 +0500

    start develop adminka

---
+++ app/admin_panel/admin_panel.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-admin_panel',
+    templateUrl: './admin_panel.component.html',
+    styleUrls: ['./admin_panel.component.css']
+})
+export class AdminComponent implements OnInit {
+    newcomponent = "Minimalistic Admen Panel";
+    constructor(){}
+    ngOnInit(){}
+}
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { InformationComponent } from './information/information.component';
@@ -11,6 +12,7 @@ import {Any_bankComponent} from "./pay/any_bank/any_bank.component";
 import {Internet_bankComponent} from "./pay/internet_bank/internet_bank.component";
 import {Bank_menuComponent} from "./pay/bank_menu/bank_menu.component";
 import {CompanyComponent} from "./company/company.component";
+import {AdminComponent} from "./admin_panel/admin_panel.component";
 
 @NgModule({
   declarations: [
@@ -22,11 +24,17 @@ import {CompanyComponent} from "./company/company.component";
     PayComponent,
     Bank_menuComponent,
     Any_bankComponent,
-    Internet_bankComponent
+    Internet_bankComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([{
+        path: "admin",
+        component: AdminComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
